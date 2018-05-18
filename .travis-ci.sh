e1bbcbb5904ba137fda2cf064215ff26d75637fd
David Allsopp <david.allsopp@metastack.com>
2018-05-18 11:09:38 +0100

    Test the backends on Travis CI macOS

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -5,6 +5,14 @@ case $TRAVIS_OS_NAME in
     sudo apt-get install coinor-libclp-dev coinor-libcbc-dev coinor-libsymphony-dev mccs
     ;;
   osx)
+    if [ -n "$MCCS_BACKENDS" ] ; then
+      rm /usr/local/include/c++
+      brew tap coin-or-tools/coinor
+      BACKEND_PACKAGES="cbc clp symphony"
+    else
+      BACKEND_PACKAGES=
+    fi
+    brew install diffutils $BACKEND_PACKAGES
     ;;
   *)
     echo "Unknown TRAVIS_OS_NAME: $TRAVIS_OS_NAME" >&2
